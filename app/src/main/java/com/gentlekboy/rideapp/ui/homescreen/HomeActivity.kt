a0053a19c351fe13469b603f220e46e4c52a8406
gentlekboy <gentlekboy@gmail.com>
2022-03-14 17:55:07 +0100

    set up view binding in home activity

--- app/src/main/java/com/gentlekboy/rideapp/ui/homescreen/HomeActivity.kt
+++ app/src/main/java/com/gentlekboy/rideapp/ui/homescreen/HomeActivity.kt
@@ -1,11 +1,17 @@
 package com.gentlekboy.rideapp.ui.homescreen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gentlekboy.rideapp.databinding.ActivityHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
