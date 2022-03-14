4dca4679117ae345ac60a42c6855c36db19a06d3
gentlekboy <gentlekboy@gmail.com>
2022-03-14 16:31:18 +0100

    set up application class

---
+++ app/src/main/java/com/gentlekboy/rideapp/app/EdvoraApp.kt
@@ -0,0 +1,12 @@
+package com.gentlekboy.rideapp.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+/**
+ * This is the [Application] class for this app
+ */
+class EdvoraApp: Application() {
+
+}
