cced4296d3394b0b386c8d0585baf007709af6ac
gentlekboy <gentlekboy@gmail.com>
2022-03-14 22:34:03 +0100

    created adapter for view pager

---
+++ app/src/main/java/com/gentlekboy/rideapp/ui/homescreen/PagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.gentlekboy.rideapp.ui.homescreen
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+    var fragmentList = ArrayList<Fragment>()
+    var titleList = ArrayList<String>()
+
+    override fun getItem(position: Int) = fragmentList[position]
+    override fun getPageTitle(position: Int) = titleList[position]
+    override fun getCount() = fragmentList.size
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+}
