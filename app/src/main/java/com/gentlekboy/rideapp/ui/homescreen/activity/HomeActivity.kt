912c752449891e932137d5a8fe87a695f0abcea6
gentlekboy <gentlekboy@gmail.com>
2022-03-15 17:21:59 +0100

    displayed user data and fetched ride data

---
+++ app/src/main/java/com/gentlekboy/rideapp/ui/homescreen/activity/HomeActivity.kt
@@ -0,0 +1,84 @@
+package com.gentlekboy.rideapp.ui.homescreen.activity
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.gentlekboy.rideapp.databinding.ActivityHomeBinding
+import com.gentlekboy.rideapp.model.data.Status
+import com.gentlekboy.rideapp.ui.homescreen.ViewPagerAdapter
+import com.gentlekboy.rideapp.ui.homescreen.adapter.RideAdapter
+import com.gentlekboy.rideapp.ui.homescreen.fragment.NearestFragment
+import com.gentlekboy.rideapp.ui.homescreen.fragment.PastFragment
+import com.gentlekboy.rideapp.ui.homescreen.fragment.UpcomingFragment
+import com.gentlekboy.rideapp.viewmodel.HomeViewModel
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var viewPagerAdapter: ViewPagerAdapter
+    private val homeViewModel: HomeViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpViewPager()
+        setUpTabLayout()
+        fetchRideData()
+        displayUserInfo()
+    }
+
+    private fun displayUserInfo() {
+        homeViewModel.fetchUserData()
+        homeViewModel.userLivedata.observe(this) { response ->
+            when (response.status) {
+                Status.SUCCESS -> {
+                    val userInfo = response.data
+
+                    if (userInfo != null) {
+                        binding.apply {
+                            userName.text = userInfo.name
+                            Glide.with(this@HomeActivity).load(userInfo.url).into(userImage)
+                        }
+                    }
+                }
+                Status.ERROR -> {
+                    Snackbar.make(binding.root, "Something went wrong", Snackbar.LENGTH_LONG).show()
+                }
+                Status.LOADING -> {}
+            }
+
+        }
+    }
+
+    private fun fetchRideData() {
+        homeViewModel.fetchRideData()
+    }
+
+    private fun setUpTabLayout() {
+        binding.apply {
+            viewPager.adapter = viewPagerAdapter
+            tabLayout.setupWithViewPager(binding.viewPager)
+            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {}
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+        }
+    }
+
+    private fun setUpViewPager() {
+        viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.apply {
+            addFragment(NearestFragment(), "Nearest")
+            addFragment(UpcomingFragment(), "Upcoming")
+            addFragment(PastFragment(), "Past")
+        }
+    }
+}
